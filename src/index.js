c0a03239f3f2ae94e6e9ad0479b718584cb22c19
MsAikho <stuurmanakhona4@gmail.com>
2024-12-08 12:02:42 +0200

    add to html netlify and css on javascript

--- src/index.js
+++ src/index.js
@@ -16,12 +16,12 @@ function generatorForm(event) {
   let apiKey = "32b37e6231bf7tb9f483785a00o2c334";
   let prompt = `User instruction:Generate a French poem about ${instructionInput.value}`;
   let context =
-    "You are a romentic poem expect and love to write short poem.your mission is to generator a 4 lines poem in basic HTML .make sure to follow the user instruction. Do not include a tittle to the poem.";
+    "You are a romentic poem expect and love to write short poem.your mission is to generator a 4 lines poem in basic HTML.make sure to follow the user instruction. Do not include a tittle to the poem. Sign the poem with 'SheCodes AI'inside a <strong>element</strong>";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=32b37e6231bf7tb9f483785a00o2c334`;
 
   let poemElement = document.querySelector("#poems");
   poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `⌛Generating a French poem ${instructionInput.value}`;
+  poemElement.innerHTML = `<div class="blink">⌛Generating a French poem ${instructionInput.value}</div>`;
 
   console.log("Generating poem");
   console.log(`Prompt${prompt}`);
